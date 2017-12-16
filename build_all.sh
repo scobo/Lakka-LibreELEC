cc4b121731e7119ec502e9dbb83797f4be780ffc
D Scott Boyce <scobo@users.noreply.github.com>
2017-12-16 12:32:52 -0800

    Replace Esper with Lakka

--- build_all.sh
+++ build_all.sh
@@ -3,65 +3,65 @@
 rm -rf target/
 
 >&2 echo "Generic.x86_64"
-DISTRO=Esper PROJECT=Generic ARCH=x86_64 make image -j8
+DISTRO=Lakka PROJECT=Generic ARCH=x86_64 make image -j8
 >&2 echo "Generic.i386"
-DISTRO=Esper PROJECT=Generic ARCH=i386 make image -j8
+DISTRO=Lakka PROJECT=Generic ARCH=i386 make image -j8
 >&2 echo "RPi.arm + noobs"
-DISTRO=Esper PROJECT=RPi ARCH=arm make noobs -j8
+DISTRO=Lakka PROJECT=RPi ARCH=arm make noobs -j8
 >&2 echo "RPi2.arm + noobs"
-DISTRO=Esper PROJECT=RPi2 ARCH=arm make noobs -j8
+DISTRO=Lakka PROJECT=RPi2 ARCH=arm make noobs -j8
 >&2 echo "Allwinner.arm"
-DISTRO=Esper PROJECT=Allwinner SYSTEM=Bananapi ARCH=arm make image -j8
-DISTRO=Esper PROJECT=Allwinner SYSTEM=Cubieboard2 ARCH=arm make image -j8
-DISTRO=Esper PROJECT=Allwinner SYSTEM=Cubietruck ARCH=arm make image -j8
-DISTRO=Esper PROJECT=Allwinner SYSTEM=orangepi_2 ARCH=arm make image -j8
-DISTRO=Esper PROJECT=Allwinner SYSTEM=orangepi_lite ARCH=arm make image -j8
-DISTRO=Esper PROJECT=Allwinner SYSTEM=orangepi_one ARCH=arm make image -j8
-DISTRO=Esper PROJECT=Allwinner SYSTEM=orangepi_pc ARCH=arm make image -j8
-DISTRO=Esper PROJECT=Allwinner SYSTEM=orangepi_plus ARCH=arm make image -j8
-DISTRO=Esper PROJECT=Allwinner SYSTEM=orangepi_plus2e ARCH=arm make image -j8
-DISTRO=Esper PROJECT=Allwinner SYSTEM=nanopi_m1_plus ARCH=arm make image -j8
+DISTRO=Lakka PROJECT=Allwinner SYSTEM=Bananapi ARCH=arm make image -j8
+DISTRO=Lakka PROJECT=Allwinner SYSTEM=Cubieboard2 ARCH=arm make image -j8
+DISTRO=Lakka PROJECT=Allwinner SYSTEM=Cubietruck ARCH=arm make image -j8
+DISTRO=Lakka PROJECT=Allwinner SYSTEM=orangepi_2 ARCH=arm make image -j8
+DISTRO=Lakka PROJECT=Allwinner SYSTEM=orangepi_lite ARCH=arm make image -j8
+DISTRO=Lakka PROJECT=Allwinner SYSTEM=orangepi_one ARCH=arm make image -j8
+DISTRO=Lakka PROJECT=Allwinner SYSTEM=orangepi_pc ARCH=arm make image -j8
+DISTRO=Lakka PROJECT=Allwinner SYSTEM=orangepi_plus ARCH=arm make image -j8
+DISTRO=Lakka PROJECT=Allwinner SYSTEM=orangepi_plus2e ARCH=arm make image -j8
+DISTRO=Lakka PROJECT=Allwinner SYSTEM=nanopi_m1_plus ARCH=arm make image -j8
 >&2 echo "imx6.cuboxi.arm"
-DISTRO=Esper PROJECT=imx6 SYSTEM=cuboxi ARCH=arm make image -j8
+DISTRO=Lakka PROJECT=imx6 SYSTEM=cuboxi ARCH=arm make image -j8
 >&2 echo "imx6.udoo.arm"
-DISTRO=Esper PROJECT=imx6 SYSTEM=udoo ARCH=arm make image -j8
+DISTRO=Lakka PROJECT=imx6 SYSTEM=udoo ARCH=arm make image -j8
 >&2 echo "OdroidC1.arm"
-DISTRO=Esper PROJECT=OdroidC1 ARCH=arm make image -j8
+DISTRO=Lakka PROJECT=OdroidC1 ARCH=arm make image -j8
 >&2 echo "Odroid_C2.arm"
-DISTRO=Esper PROJECT=Odroid_C2 ARCH=arm make image -j8
+DISTRO=Lakka PROJECT=Odroid_C2 ARCH=arm make image -j8
 >&2 echo "OdroidXU3.arm"
-DISTRO=Esper PROJECT=OdroidXU3 ARCH=arm make image -j8
+DISTRO=Lakka PROJECT=OdroidXU3 ARCH=arm make image -j8
 >&2 echo "WeTek_Core.arm"
-DISTRO=Esper PROJECT=WeTek_Core ARCH=arm make image -j8
+DISTRO=Lakka PROJECT=WeTek_Core ARCH=arm make image -j8
 >&2 echo "WeTek_Hub.arm"
-DISTRO=Esper PROJECT=WeTek_Hub ARCH=arm make image -j8
+DISTRO=Lakka PROJECT=WeTek_Hub ARCH=arm make image -j8
 >&2 echo "WeTek_Play.arm"
-DISTRO=Esper PROJECT=WeTek_Play ARCH=arm make image -j8
+DISTRO=Lakka PROJECT=WeTek_Play ARCH=arm make image -j8
 >&2 echo "WeTek_Play_2.arm"
-DISTRO=Esper PROJECT=WeTek_Play_2 ARCH=arm make image -j8
+DISTRO=Lakka PROJECT=WeTek_Play_2 ARCH=arm make image -j8
 >&2 echo "Gamegirl.arm"
-DISTRO=Esper PROJECT=Gamegirl ARCH=arm make image -j8
+DISTRO=Lakka PROJECT=Gamegirl ARCH=arm make image -j8
 >&2 echo "S8X2.arm"
-DISTRO=Esper PROJECT=S8X2 SYSTEM=S82 ARCH=arm make image -j8
-DISTRO=Esper PROJECT=S8X2 SYSTEM=M8 ARCH=arm make image -j8
-DISTRO=Esper PROJECT=S8X2 SYSTEM=T8 ARCH=arm make image -j8
-DISTRO=Esper PROJECT=S8X2 SYSTEM=MXIII-1G ARCH=arm make image -j8
-DISTRO=Esper PROJECT=S8X2 SYSTEM=MXIII-PLUS ARCH=arm make image -j8
-DISTRO=Esper PROJECT=S8X2 SYSTEM=X8H-PLUS ARCH=arm make image -j8
+DISTRO=Lakka PROJECT=S8X2 SYSTEM=S82 ARCH=arm make image -j8
+DISTRO=Lakka PROJECT=S8X2 SYSTEM=M8 ARCH=arm make image -j8
+DISTRO=Lakka PROJECT=S8X2 SYSTEM=T8 ARCH=arm make image -j8
+DISTRO=Lakka PROJECT=S8X2 SYSTEM=MXIII-1G ARCH=arm make image -j8
+DISTRO=Lakka PROJECT=S8X2 SYSTEM=MXIII-PLUS ARCH=arm make image -j8
+DISTRO=Lakka PROJECT=S8X2 SYSTEM=X8H-PLUS ARCH=arm make image -j8
 >&2 echo "S805.arm"
-DISTRO=Esper PROJECT=S805 SYSTEM=MXQ ARCH=arm make image -j8
-DISTRO=Esper PROJECT=S805 SYSTEM=HD18Q ARCH=arm make image -j8
-DISTRO=Esper PROJECT=S805 SYSTEM=M201C ARCH=arm make image -j8
-DISTRO=Esper PROJECT=S805 SYSTEM=M201D ARCH=arm make image -j8
-DISTRO=Esper PROJECT=S805 SYSTEM=MK808B-Plus ARCH=arm make image -j8
+DISTRO=Lakka PROJECT=S805 SYSTEM=MXQ ARCH=arm make image -j8
+DISTRO=Lakka PROJECT=S805 SYSTEM=HD18Q ARCH=arm make image -j8
+DISTRO=Lakka PROJECT=S805 SYSTEM=M201C ARCH=arm make image -j8
+DISTRO=Lakka PROJECT=S805 SYSTEM=M201D ARCH=arm make image -j8
+DISTRO=Lakka PROJECT=S805 SYSTEM=MK808B-Plus ARCH=arm make image -j8
 >&2 echo "S905.arm"
-DISTRO=Esper PROJECT=S905 ARCH=arm make image -j8
+DISTRO=Lakka PROJECT=S905 ARCH=arm make image -j8
 >&2 echo "TinkerBoard.arm"
-#DISTRO=Esper PROJECT=Rockchip DEVICE=TinkerBoard ARCH=arm make image -j8
+#DISTRO=Lakka PROJECT=Rockchip DEVICE=TinkerBoard ARCH=arm make image -j8
 >&2 echo "ROCK64.arm"
-#DISTRO=Esper PROJECT=Rockchip DEVICE=ROCK64 ARCH=arm make image -j8
+#DISTRO=Lakka PROJECT=Rockchip DEVICE=ROCK64 ARCH=arm make image -j8
 #>&2 echo "MiQi.arm"
-DISTRO=Esper PROJECT=Rockchip DEVICE=MiQi ARCH=arm make image -j8
+DISTRO=Lakka PROJECT=Rockchip DEVICE=MiQi ARCH=arm make image -j8
 
 rm target/*.kernel
 rm target/*.system
@@ -73,7 +73,7 @@ done
 
 for f in target/*; do
   #dir=`echo $f | sed -e 's/target\/Lakka-\(.*\)-\(.*\)-devel-\(.*\)/\1/'`
-  dir=`echo $f | sed -e 's/target\/Esper-\(.*\)-\(.*\)-devel-\(.*\)/\1/'`
+  dir=`echo $f | sed -e 's/target\/Lakka-\(.*\)-\(.*\)-devel-\(.*\)/\1/'`
   #dir=`echo $f | sed -e 's/target\/Lakka-\(.*\)-2.1\(.*\)/\1/'`
   mkdir -p target/$dir
   mv $f target/$dir/
--- packages/lakka/lakka-update/sources/lakka-update.sh
+++ packages/lakka/lakka-update/sources/lakka-update.sh
@@ -3,10 +3,10 @@
 source /etc/os-release
 
 #MIRROR=http://le.builds.lakka.tv
-MIRROR=http://scottpowered.com/esper
+MIRROR=http://scottpowered.com/lakka
 
 FILE=`wget $MIRROR/${LIBREELEC_ARCH}/.index -q -O - | head -1`
-#FILE=Lakka-RPi2.arm-esper.img.gz
+#FILE=Lakka-RPi2.arm-lakka.img.gz
 #URL=$MIRROR/${LIBREELEC_ARCH}/$FILE
 #URL=https://github.com/scobo/Lakka-LibreELEC/archive/$FILE
 URL=https://github.com/scobo/Lakka-LibreELEC/releases/download/$FILE
