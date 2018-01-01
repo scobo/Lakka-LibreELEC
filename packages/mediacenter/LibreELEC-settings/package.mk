eef2cbc01af4acd8f6e873d2807e288300cda187
scobo <scobo@users.noreply.github.com>
2018-01-01 10:37:53 -0800

    Update LibreELEC-settings to 0.8.11

--- packages/mediacenter/LibreELEC-settings/package.mk
+++ packages/mediacenter/LibreELEC-settings/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="LibreELEC-settings"
-PKG_VERSION="12c5f24"
+PKG_VERSION="0.8.11"
 PKG_ARCH="any"
 PKG_LICENSE="prop."
 PKG_SITE="https://libreelec.tv"
