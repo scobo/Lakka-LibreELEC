f92f3dc9babcd1afac4be21e63219d62dcb321a4
scobo <scobo@users.noreply.github.com>
2018-01-01 20:32:28 -0800

    Update bluemsx

--- packages/libretro/bluemsx/package.mk
+++ packages/libretro/bluemsx/package.mk
@@ -19,8 +19,7 @@
 ################################################################################
 
 PKG_NAME="bluemsx"
-PKG_VERSION="e0ac210"
-PKG_REV="1"
+PKG_VERSION="8268f7f"
 PKG_ARCH="any"
 PKG_LICENSE="GPLv2"
 PKG_SITE="https://github.com/libretro/blueMSX-libretro"
