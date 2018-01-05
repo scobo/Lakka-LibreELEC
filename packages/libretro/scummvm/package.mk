6500d08e106c3047564ca645e626a1952cd5d0e9
scobo <scobo@users.noreply.github.com>
2018-01-05 12:18:50 -0800

    Update scummvm

--- packages/libretro/scummvm/package.mk
+++ packages/libretro/scummvm/package.mk
@@ -19,8 +19,7 @@
 ################################################################################
 
 PKG_NAME="scummvm"
-PKG_VERSION="e35edf9"
-PKG_REV="1"
+PKG_VERSION="6345d9a"
 PKG_ARCH="any"
 PKG_LICENSE="GPLv2"
 PKG_SITE="https://github.com/libretro/scummvm"
