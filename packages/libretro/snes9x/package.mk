b2a6d72ac5894027ff2e1d256965dd2b87e5748f
scobo <scobo@users.noreply.github.com>
2018-01-05 12:19:46 -0800

    Update snes9x

--- packages/libretro/snes9x/package.mk
+++ packages/libretro/snes9x/package.mk
@@ -19,8 +19,7 @@
 ################################################################################
 
 PKG_NAME="snes9x"
-PKG_VERSION="eb7cced"
-PKG_REV="1"
+PKG_VERSION="18b5fee"
 PKG_ARCH="any"
 PKG_LICENSE="Non-commercial"
 PKG_SITE="https://github.com/libretro/snes9x"
