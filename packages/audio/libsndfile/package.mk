3e3478eafd61db636ab55296af771cbb8d9ad49c
scobo <scobo@users.noreply.github.com>
2018-01-06 19:00:51 -0800

    Update libsndfile to 1.0.28

--- packages/audio/libsndfile/package.mk
+++ packages/audio/libsndfile/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="libsndfile"
-PKG_VERSION="1.0.27"
+PKG_VERSION="1.0.28"
 PKG_ARCH="any"
 PKG_LICENSE="LGPL"
 PKG_SITE="http://www.mega-nerd.com/libsndfile/"
