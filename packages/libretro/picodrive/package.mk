b7e5ab572a0e816d5a6b11eac55bcd2fa1cac967
scobo <scobo@users.noreply.github.com>
2018-01-06 20:40:08 -0800

    Update picodrive

--- packages/libretro/picodrive/package.mk
+++ packages/libretro/picodrive/package.mk
@@ -19,7 +19,7 @@
 ################################################################################
 
 PKG_NAME="picodrive"
-PKG_VERSION="8581be5"
+PKG_VERSION="007cb5a"
 PKG_ARCH="any"
 PKG_LICENSE="MAME"
 PKG_SITE="https://github.com/libretro/picodrive"
