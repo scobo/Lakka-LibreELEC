08bf56d334bb03a5c41890312b57d480cfe4d6ac
scobo <scobo@users.noreply.github.com>
2018-01-07 17:51:21 -0800

    Update gptfdisk to 1.0.3

--- packages/sysutils/gptfdisk/package.mk
+++ packages/sysutils/gptfdisk/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="gptfdisk"
-PKG_VERSION="1.0.1"
+PKG_VERSION="1.0.3"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
 PKG_SITE="http://www.rodsbooks.com/gdisk/"
