c9d943ca54692640f2ce35cf3b8a2f3b0f8316e7
scobo <scobo@users.noreply.github.com>
2018-01-22 16:47:02 -0800

    Update 81 and add to RPi3 project

--- packages/libretro/81/package.mk
+++ packages/libretro/81/package.mk
@@ -19,8 +19,7 @@
 ################################################################################
 
 PKG_NAME="81"
-PKG_VERSION="d972a77"
-PKG_REV="1"
+PKG_VERSION="6ae21e0"
 PKG_ARCH="any"
 PKG_LICENSE="GPLv3"
 PKG_SITE="https://github.com/libretro/81-libretro"
