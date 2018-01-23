766056bccb2b0c046203e22ac38c61e22990e2df
scobo <scobo@users.noreply.github.com>
2018-01-22 17:21:48 -0800

    Update pocketcdg and add to RPi3 project

--- packages/libretro/pocketcdg/package.mk
+++ packages/libretro/pocketcdg/package.mk
@@ -19,8 +19,7 @@
 ################################################################################
 
 PKG_NAME="pocketcdg"
-PKG_VERSION="cba7d73"
-PKG_REV="1"
+PKG_VERSION="2697897"
 PKG_ARCH="any"
 PKG_LICENSE="MIT"
 PKG_SITE="https://github.com/libretro/libretro-pocketcdg"
