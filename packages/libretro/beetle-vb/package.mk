625871609392ef96c567954f183219a3a3a90347
scobo <scobo@users.noreply.github.com>
2018-01-22 16:55:47 -0800

    Update beetle-vb and add to RPi3 project

--- packages/libretro/beetle-vb/package.mk
+++ packages/libretro/beetle-vb/package.mk
@@ -19,8 +19,7 @@
 ################################################################################
 
 PKG_NAME="beetle-vb"
-PKG_VERSION="1e4a1cb"
-PKG_REV="1"
+PKG_VERSION="49eee8e"
 PKG_ARCH="any"
 PKG_LICENSE="GPLv2"
 PKG_SITE="https://github.com/libretro/beetle-vb-libretro"
