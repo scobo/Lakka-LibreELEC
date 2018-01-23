9303d3fbdf2d9ec2612d30810058b7d11d6823f5
scobo <scobo@users.noreply.github.com>
2018-01-22 17:16:48 -0800

    Update lutro and add to RPi3 project

--- packages/libretro/lutro/package.mk
+++ packages/libretro/lutro/package.mk
@@ -19,12 +19,12 @@
 ################################################################################
 
 PKG_NAME="lutro"
-PKG_VERSION="d9cacd3"
-PKG_REV="1"
+PKG_VERSION="128153c"
 PKG_ARCH="any"
 PKG_LICENSE="MIT"
 PKG_SITE="https://github.com/libretro/libretro-lutro"
-PKG_URL="https://github.com/libretro/libretro-lutro/archive/master.tar.gz"
+PKG_URL="https://github.com/libretro/libretro-lutro/archive/$PKG_VERSION.tar.gz"
+PKG_SOURCE_DIR="libretro-lutro-$PKG_VERSION*"
 PKG_DEPENDS_TARGET="toolchain"
 PKG_PRIORITY="optional"
 PKG_SECTION="libretro"
