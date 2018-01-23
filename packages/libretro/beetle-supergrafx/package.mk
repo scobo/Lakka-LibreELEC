eca479053a11842307ab5b5ad6f923a94db6cf45
scobo <scobo@users.noreply.github.com>
2018-01-22 16:54:48 -0800

    Update beetle-supergrafx and add to RPi3 project

--- packages/libretro/beetle-supergrafx/package.mk
+++ packages/libretro/beetle-supergrafx/package.mk
@@ -19,8 +19,7 @@
 ################################################################################
 
 PKG_NAME="beetle-supergrafx"
-PKG_VERSION="c9634b2"
-PKG_REV="1"
+PKG_VERSION="56e7864"
 PKG_ARCH="any"
 PKG_LICENSE="GPLv2"
 PKG_SITE="https://github.com/libretro/beetle-supergrafx-libretro"
