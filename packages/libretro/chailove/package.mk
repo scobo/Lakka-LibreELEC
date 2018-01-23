ceff27d4ddf19bb4ba08cf691a0748860445f37c
scobo <scobo@users.noreply.github.com>
2018-01-22 16:58:23 -0800

    Update chailove and add to RPi3 project

--- packages/libretro/chailove/package.mk
+++ packages/libretro/chailove/package.mk
@@ -19,8 +19,7 @@
 ################################################################################
 
 PKG_NAME="chailove"
-PKG_VERSION="b9ef6df"
-PKG_REV="1"
+PKG_VERSION="2fa61b7"
 PKG_ARCH="any"
 PKG_LICENSE="MIT"
 PKG_SITE="https://github.com/RobLoach/ChaiLove"
